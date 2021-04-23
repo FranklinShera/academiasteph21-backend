870d3a704d156a1e8cf6903003105ba474cc84fc
FranklinShera <fshera96@gmail.com>
2021-04-24 00:14:41 +0300

    BCRYPT

--- Helpers/Hashing.js
+++ Helpers/Hashing.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcrypt')
 
+
 module.exports = {
     hashPassword : function(){
         return "Hashing Password"
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const bodyParser =  require('body-parser')
 const cors = require('cors')
 
-const hashHelper = require('./Helpers/Hashing')
+const bcrypt = require('bcrypt')
 
 const app = express();
 
@@ -15,38 +15,36 @@ app.use(bodyParser.json());
 router.post('/auth/login', async (req, res) => {
     try{
 
-        User.findAll({ where:{ email : req.body.username}}).then((users) => {
+            let user = await User.findOne({ where:{ email : req.body.username}})
             
-            if(users.length == 0){
-                res.send({ error : "Invalid Credentials"})
+            if(user == null){
+                res.status(404).send({ error : "User Not Found!"})
             }else{
-                (users[0].password === req.body.password) ? res.send({ auth: true ,
+                (bcrypt.compare(req.body.password,user.password)) ? res.send({ auth: true ,
                             message: "Logged In Successfully" ,
-                            user:{ id: users[0].id , name: users[0].name , email: users[0].email }
-                     })  : res.send({ error : "Invalid Credentials"})
+                            user:{ id: user.id , name: user.name , email: user.email }
+                     })  : res.status(404).send({ error : "Invalid Credentials"})
             }
-        }).catch((err) => {
-            res.send({ error : err})
-        }) 
+        
 
     } catch(err){
-        res.send({error : err})
+        res.status(404).send({error : err})
     }  
 
 })
 
 router.post('/auth/register', async (req, res) => {
     try{
-        // User.create({
-        //     name : "Franklin Shera",
-        //     email : req.body.username,
-        //     password : req.body.password
-        // }).catch((err) => {
-        //     console.log(err);
-        // })
+        User.create({
+            name : req.body.name,
+            email : req.body.email,
+            password : await bcrypt.hash(req.body.password, 7)
+        }).catch((err) => {
+            res.status(404).send({error: err});
+        })
         
     
-        res.send(hashHelper.hashPassword())   
+        res.status(201).send({ message: "Registration Successful!"})   
 
     } catch(err){
         res.send({error : err})
